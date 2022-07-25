28aa9e5cd16fe0be23009a311ebd048e82c37784
Stephanos <stephanos@Stephanoss-Mac-mini.local>
2022-07-25 18:39:17 +0300

    add parameterised filters

--- functions/fetch-sso.js
+++ functions/fetch-sso.js
@@ -11,10 +11,10 @@ export const handler = requireAuth(async (event, context) => {
 
   try {
     const { claims } = context.identityContext;
-
+    console.log("SHOP IN CLAIM ISSS: ", claims.data.shop_name);
     const generateSSOcredentials = async () => {
       return await client.create("authorization", {
-        integration_id: "3aaf834e-6f97-4204-81aa-e3215811bef8",
+        integration_id: "5bdf0a8f-3c2b-4214-97b0-45c0dc0ac8f7",
         type: "sso",
         expiry: "24 hours",
         inactivity_interval: "10 minutes",
@@ -23,6 +23,9 @@ export const handler = requireAuth(async (event, context) => {
         email: "example@app.com",
         suborganization: "exampleUser",
         role: "viewer",
+        metadata: {
+          shop_name: [claims.data.shop_name],
+        },
         theme: {
           id: claims.preferences.theme,
           type: "foo",
--- src/components/CumulWrapper.js
+++ src/components/CumulWrapper.js
@@ -8,7 +8,7 @@ export const CumulioWrapper = ({ dashboardKey, dashboardToken }) => {
       ref={ref}
       authKey={dashboardKey}
       authToken={dashboardToken}
-      dashboardId="91ef0336-ec31-48c1-8f25-d28d603b82ec"
+      dashboardId="97449715-61ed-4c04-80cd-98b9b84c884d"
       switchScreenModeOnResize={false}
       loaderSpinnerColor="rgb(0, 81, 126)"
       loaderSpinnerBackground="rgb(236 248 255)"
